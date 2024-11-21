cd1b2b76e5842804b9476ca51a74c50431aca593
GiancarloCiulla <giancarlociulla.alfano@gmail.com>
2024-11-21 12:24:13 +0100

    add/solution

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,28 @@
+
+import React, { useState } from "react";
+
+const SearchForm = ({ onCityChange }) => {
+  const [inputValue, setInputValue] = useState("Madrid");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      onCityChange(inputValue);
+      setInputValue(""); // Limpia el input
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchForm;
