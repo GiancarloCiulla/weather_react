ec726c33d2d758cd5ef5a8c52d1e4680d21af13e
GiancarloCiulla <giancarlociulla.alfano@gmail.com>
2025-01-12 19:06:21 +0100

    add/style

--- src/components/WeatherList/WeatherList.jsx
+++ src/components/WeatherList/WeatherList.jsx
@@ -44,8 +44,8 @@ const WeatherList = ({ weatherData }) => {
             <strong>Min:</strong> {Math.round(minTemp)}°C
           </p>
           <p>
-            <strong>Max:</strong> {Math.round(maxTemp)}°C
-          </p>
+  <strong style={{ color: "red" }}>Max:</strong> {Math.round(maxTemp)}°C
+</p>
         </div>
       ))}
     </div>
@@ -53,75 +53,3 @@ const WeatherList = ({ weatherData }) => {
 };
 
 export default WeatherList;
-
-
-// import React from "react";
-
-// const WeatherList = ({ weatherData }) => {
-//   // Agrupamos los datos por día y calculamos las temperaturas máximas y mínimas
-//   const groupedByDay = weatherData.reduce((acc, item) => {
-//     const date = new Date(item.dt * 1000).toDateString(); // Obtenemos el día
-//     acc[date] = acc[date] || [];
-//     acc[date].push(item);
-//     return acc;
-//   }, {});
-
-//   // Convertimos los datos para mostrar solo la temperatura máxima y mínima
-//   const dailySummaries = Object.entries(groupedByDay).map(([day, items]) => {
-//     const temps = items.map((item) => item.main.temp); // Obtenemos todas las temperaturas del día
-//     return {
-//       day,
-//       minTemp: Math.min(...temps), // Temperatura mínima
-//       maxTemp: Math.max(...temps), // Temperatura máxima
-//     };
-//   });
-
-//   return (
-//     <div className="weather-list">
-//       {dailySummaries.map(({ day, minTemp, maxTemp }) => (
-//         <div key={day} className="daily-summary">
-//           <h3>{day}</h3>
-//           <p>
-//             <strong>Min:</strong> {Math.round(minTemp)}°C
-//           </p>
-//           <p>
-//             <strong>Max:</strong> {Math.round(maxTemp)}°C
-//           </p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default WeatherList;
-
-
-
-// import React from "react";
-// import WeatherCard from "../WeatherCard/WeatherCard";
-
-// const WeatherList = ({ weatherData }) => {
-//   const groupedByDay = weatherData.reduce((acc, item) => {
-//     const date = new Date(item.dt * 1000).toDateString(); // Agrupamos por fecha
-//     acc[date] = acc[date] || [];
-//     acc[date].push(item);
-//     return acc;
-//   }, {});
-
-//   return (
-//     <div className="weather-list">
-//       {Object.entries(groupedByDay).map(([day, items]) => (
-//         <div key={day}>
-//           <h3>{day}</h3>
-//           <div className="weather-cards">
-//             {items.map((item) => (
-//               <WeatherCard key={item.dt} weatherData={item} />
-//             ))}
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default WeatherList;
